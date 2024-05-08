56640ab3c0c3a98ce165a54f37d9f04d68870044
Oery <48154940+Oery@users.noreply.github.com>
2024-05-08 13:20:44 +0200

    Add Debounce Time

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
+use std::{collections::HashMap, sync::Mutex};
+
 use windows::{
     core::*,
-    Win32::{
-        Foundation::*, System::LibraryLoader::GetModuleHandleA, UI::Input::KeyboardAndMouse::*,
-        UI::WindowsAndMessaging::*,
-    },
+    Win32::{Foundation::*, System::LibraryLoader::GetModuleHandleA, UI::WindowsAndMessaging::*},
 };
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref LAST_KEY_PRESSES: Mutex<HashMap<u32, u32>> = Mutex::new(HashMap::new());
+}
+
+const DEBOUNCE_TIME: u32 = 70;
 
 unsafe extern "system" fn keyboard_hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     if code >= 0 {
         let kb_struct = *(lparam.0 as *const KBDLLHOOKSTRUCT);
+        let vk_code = kb_struct.vkCode;
 
-        println!("{:?}", kb_struct);
-
-        if kb_struct.vkCode == VK_ESCAPE.0.into() {
-            return LRESULT(1);
+        if wparam.0 == WM_KEYDOWN as usize || wparam.0 == WM_SYSKEYDOWN as usize {
+            let current_time = kb_struct.time;
+            let mut map = LAST_KEY_PRESSES.lock().unwrap();
+            if let Some(&last_press_time) = map.get(&vk_code) {
+                if current_time - last_press_time < DEBOUNCE_TIME {
+                    return LRESULT(1);
+                }
+            }
+            map.insert(vk_code, current_time);
         }
     }
     CallNextHookEx(None, code, wparam, lparam)
