536b7d48a7eac6a1d55a0e70d8d523505527970f
Oery <48154940+Oery@users.noreply.github.com>
2024-05-08 12:51:59 +0200

    Add Keys Filtering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use windows::{
+    core::*,
+    Win32::{
+        Foundation::*, System::LibraryLoader::GetModuleHandleA, UI::Input::KeyboardAndMouse::*,
+        UI::WindowsAndMessaging::*,
+    },
+};
+
+unsafe extern "system" fn keyboard_hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    if code >= 0 {
+        let kb_struct = *(lparam.0 as *const KBDLLHOOKSTRUCT);
+
+        println!("{:?}", kb_struct);
+
+        if kb_struct.vkCode == VK_ESCAPE.0.into() {
+            return LRESULT(1);
+        }
+    }
+    CallNextHookEx(None, code, wparam, lparam)
+}
+
+fn main() -> Result<()> {
+    unsafe {
+        let instance = GetModuleHandleA(None)?;
+        let hook = SetWindowsHookExA(WH_KEYBOARD_LL, Some(keyboard_hook_proc), instance, 0);
+
+        let mut msg = MSG::default();
+
+        while GetMessageA(&mut msg, HWND(0), 0, 0).into() {
+            let _ = TranslateMessage(&msg);
+            DispatchMessageA(&msg);
+        }
+
+        let _ = UnhookWindowsHookEx(hook.unwrap());
+    }
+    Ok(())
 }
